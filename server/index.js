08440b048d04229765f91ca8e05861dbd44aac93
yen <jackjack201092620@icloud.com>
2025-04-22 02:35:36 +0800

    test: DB連接成功

--- server/index.js
+++ server/index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const sopsRouter = require('./routes/sops');
+const userRouter = require('./routes/user');
+
+require('./config/db');
+
 
 app.use(cors());
 app.use('/api/sops', sopsRouter);
+app.use('/api/users', userRouter);
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
+
+
+
+
+
