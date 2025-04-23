872312ee04940dc0f087f028c45d1e6f2421d28c
Sharon Shih <sharonshih10530523@gmail.com>
2025-04-23 20:08:56 +0800

    feat: 抓取personalid並記錄瀏覽

---
+++ server/services/viewService.js
@@ -0,0 +1,24 @@
+const { v4: uuidv4 } = require('uuid');
+const { getLatestViewId, insertViewRecord } = require('../models/viewModel');
+
+const logSOPView = async (req, sopId) => {
+  let personalId;
+
+  if (req.user && req.user.id) {
+    // 有登入，不是很確定
+    personalId = req.user.id;
+  } else {
+    // 沒登入就給隨機 ID，存在 cookie
+    if (!req.cookies.personalId) {
+      personalId = `GUEST_${uuidv4()}`;
+      res.cookie('personalId', personalId, { httpOnly: true, maxAge: 31536000000 });
+    } else {
+      personalId = req.cookies.personalId;
+    }
+  }
+
+  const viewId = await getLatestViewId();
+  await insertViewRecord(viewId, sopId, personalId);
+};
+
+module.exports = { logSOPView };
