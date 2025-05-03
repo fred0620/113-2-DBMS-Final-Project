d1ec82fdb79fa32966a73376efce959d9e384b6c
polly123926 <112306031@g.nccu.edu.tw>
2025-05-04 00:33:26 +0800

    Resolve

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ require('./config/db');
 
 app.use(cors());
 app.use(express.json()); 
-app.use(express.urlencoded({ extended: true }));
+
 
 const sopsRouter = require('./routes/sops');
 const userRouter = require('./routes/user');
