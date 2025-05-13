d1f2dc85f1e3477e38be2ebef9d59d7a0e208fca
Sharon Shih <sharonshih10530523@gmail.com>
2025-05-13 12:56:03 +0800

    fix: 調格式

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,11 @@
 const db=require('../config/db')
 
-
-
-
 const checkUserByEmail = async (email) => {
     const [rows] = await db.query(`
         SELECT Personal_ID, User_Name , Password
         FROM User 
         WHERE Email = ?`, [email]);
-    return rows[0];
+    return rows;
   };
   
   module.exports = { checkUserByEmail };
