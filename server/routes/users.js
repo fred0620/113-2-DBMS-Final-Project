a9d17e87dc29fa366dc51fca5aef1c3e9665cb77
polly123926 <112306031@g.nccu.edu.tw>
2025-05-21 04:08:42 +0800

    user search+searchsop update

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { userregister } = require('../controllers/user');
+const { userregister, searchUserskeyword } = require('../controllers/user');
+
 
 router.post('/register', userregister);
 
 const auth    = require('../controllers/login');
 router.post("/login", auth.login);
-
+router.get('/search', searchUserskeyword);
 module.exports = router;
