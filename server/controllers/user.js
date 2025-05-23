c097f7462f654b58bdd88ae9190996699bdc7652
yen <jackjack201092620@icloud.com>
2025-05-24 00:24:57 +0800

    fix:search user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -39,27 +39,31 @@ const userregister = async (req, res) => {
 
 const searchUserskeyword = async (req, res) => {
   const keyword = req.query.keyword?.trim();
-  if (!keyword) {
-    return res.status(400).json({ error: '請提供 keyword 參數' });
-  }
 
   try {
-    const [rows] = await db.query(
-      `SELECT User.Personal_ID, User.User_Name
-FROM User
-JOIN Administrator ON User.Personal_ID = Administrator.Personal_ID
-JOIN Team ON Administrator.Team_ID = Team.Team_ID
-WHERE User.User_Name LIKE ? OR Team.Team_Name LIKE ?
-`,
-      [`%${keyword}%`, `%${keyword}%`]
-    );
+    let query = `
+      SELECT U.Personal_ID, U.User_Name, A.Administrator_ID, U.Email, A.Ex_number, T.Team_Name, D.Department_Name
+      FROM User U
+      JOIN Administrator A ON U.Personal_ID = A.Personal_ID
+      JOIN Team T ON A.Team_ID = T.Team_ID
+      JOIN Department D ON T.Department_ID = D.Department_ID
+    `;
+    let params = [];
 
+    // 如果有 keyword 才加條件
+    if (keyword) {
+      query += ` WHERE U.User_Name LIKE ? OR T.Team_Name LIKE ? OR D.Department_Name LIKE ?`;
+      params = [`%${keyword}%`, `%${keyword}%`, `%${keyword}%`];
+    }
+
+    const [rows] = await db.query(query, params);
     res.json({ status: 'success', data: rows });
   } catch (err) {
     console.error('[SEARCH_USER_ERROR]', err);
     res.status(500).json({ status: 'error', message: '伺服器錯誤', detail: err.message });
   }
 };
+
 
   
 
