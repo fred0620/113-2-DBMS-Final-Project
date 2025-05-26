37c53ca840380e0487f444d1b655af4202223b07
MurielShih <162577341+MurielShih@users.noreply.github.com>
2025-05-26 12:31:55 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,21 +3,28 @@ export default function Footer() {
     <footer className="bg-primary text-white">
       <div className="w-full px-8 py-8 flex flex-wrap justify-between gap-6">
         {/* 關於我們 */}
-        <div className="min-w-[150px] text-left">
+        <div className="min-w-[150px] text-left leading-6">
           <a href="#" className="hover:underline whitespace-nowrap">
             關於我們
           </a>
+          <p>負責人: 吳彥勳</p>
+          <p>email:110207317@g.nccu.edu.tw </p>
         </div>
 
         {/* 使用條款 */}
-        <div className="min-w-[150px] text-left">
-          <a href="#" className="hover:underline whitespace-nowrap">
-            本網站著作權屬於國立政治大學 
+        <div className="min-w-[120px] text-left">
+          <a href="https://www.nccu.edu.tw/p/426-1000-30.php?Lang=zh-tw" className="hover:underline whitespace-nowrap">
+             尊重智慧財產權
           </a>
+          <a> | </a>
+          <a href="https://www.nccu.edu.tw/p/426-1000-25.php?Lang=zh-tw" className="hover:underline whitespace-nowrap">
+             網路使用規範要點
+          </a>
+          <p>本網站著作權屬於國立政治大學</p>
         </div>
 
         {/* 政大首頁連結 */}
-        <div className="min-w-[200px] text-left">
+        <div className="min-w-[120px] text-left">
           <a
             href="https://www.nccu.edu.tw"
             target="_blank"
